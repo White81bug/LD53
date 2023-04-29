9a5185919b68cefdbeac4cf90d7a26a84ee3581f
Alfie213 <arturbogoratov@gmail.com>
2023-04-29 13:19:29 +0300

    Fixed PlayerCollect

--- Assets/Scripts/Enviroment/Collectables/PlayerCollect.cs
+++ Assets/Scripts/Enviroment/Collectables/PlayerCollect.cs
@@ -1,20 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
 /// Player interaction with ICollectable.
 /// </summary>
+[RequireComponent(typeof(Collider))]
 public class PlayerCollect : MonoBehaviour
 {
-    private ICollectable _collectableObject;
+    private List<GameObject> _collectableObjects = new List<GameObject>(); // List<> to prevent bug with several GameObjects collision.
 
-    public void Take()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CollectableObject>())
+        {
+            _collectableObjects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<CollectableObject>())
+        {
+            _collectableObjects.Remove(other.gameObject);
+        }
+    }
+
+    private void Take()
     {
         throw new System.NotImplementedException();
     }
 
-    public void Drop()
+    private void Drop()
     {
         throw new System.NotImplementedException();
     }
