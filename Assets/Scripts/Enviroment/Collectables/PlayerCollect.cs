524b2dfae1be2ea773ec8eea5f1b3f88eba57976
Alfie213 <arturbogoratov@gmail.com>
2023-04-29 14:26:40 +0300

    CollectableSystem Take() & Drop()

--- Assets/Scripts/Enviroment/Collectables/PlayerCollect.cs
+++ Assets/Scripts/Enviroment/Collectables/PlayerCollect.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
@@ -8,13 +7,37 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class PlayerCollect : MonoBehaviour
 {
-    private List<GameObject> _collectableObjects = new List<GameObject>(); // List<> to prevent bug with several GameObjects collision.
+    [SerializeField] private Transform _pointOfObject;
+
+    private List<GameObject> _collectableObjects; // List<> to prevent bug with several GameObjects collision.
+    private GameObject _currentObject;
+
+    private void Awake()
+    {
+        _collectableObjects = new List<GameObject>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Take();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Drop();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CollectableObject>())
         {
-            _collectableObjects.Add(other.gameObject);
+            if (!_collectableObjects.Contains(other.gameObject))
+            {
+                _collectableObjects.Add(other.gameObject);
+                Debug.Log("Added");
+            }
         }
     }
 
@@ -23,16 +46,31 @@ public class PlayerCollect : MonoBehaviour
         if (other.GetComponent<CollectableObject>())
         {
             _collectableObjects.Remove(other.gameObject);
+            Debug.Log("Removed");
         }
     }
 
     private void Take()
     {
-        throw new System.NotImplementedException();
+        if (_currentObject is null && _collectableObjects.Count >= 1)
+        {
+            _currentObject = AdditionalMath.FindClosestGameObject(transform, _collectableObjects);
+            _currentObject.transform.position = _pointOfObject.transform.position;
+            _currentObject.transform.SetParent(_pointOfObject);
+        }
     }
 
     private void Drop()
     {
-        throw new System.NotImplementedException();
+        if (_currentObject is not null)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_currentObject.transform.position, Vector3.down, out hit))
+                _currentObject.transform.position = hit.point;
+            else Debug.LogError("Physics.Raycast didn't found floor.");
+
+            _currentObject.transform.SetParent(null);
+            _currentObject = null;
+        }
     }
 }
--- Assets/Scripts/Math/AdditionalMath.cs
+++ Assets/Scripts/Math/AdditionalMath.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class AdditionalMath
@@ -17,5 +18,20 @@ public static class AdditionalMath
             }
         }
         return closestTransform;
+    }
+
+    public static Transform FindClosestTransform(Transform position, List<GameObject> gameObjects)
+    {
+        Transform[] transforms = new Transform[gameObjects.Count];
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            transforms[i] = gameObjects[i].GetComponent<Transform>();
+        }
+        return FindClosestTransform(position, transforms);
+    }
+
+    public static GameObject FindClosestGameObject(Transform position, List<GameObject> gameObjects)
+    {
+        return FindClosestTransform(position, gameObjects).gameObject;
     }
 }
