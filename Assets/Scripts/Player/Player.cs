17c34470197b6c5569eaa9069a5a459c7962bc5d
Alfie213 <arturbogoratov@gmail.com>
2023-04-29 14:50:56 +0300

    Commented Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,36 +3,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private List<CollectableObject> _collectables;
-    private Transform collectoblesParent;
-    private MovementController _movementController;
-    // Start is called before the first frame update
-    void Start()
-    {
-        collectoblesParent = transform.GetChild(0);
-    }
+    //private List<CollectableObject> _collectables;
+    //private Transform collectoblesParent;
+    //private MovementController _movementController;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    collectoblesParent = transform.GetChild(0);
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 
-    public void TakeCollectable(CollectableObject collectable)
-    {
-        _collectables.Add(collectable);
-        collectable.transform.SetParent(collectoblesParent);
-        collectable.transform.localPosition = new Vector3(0,_collectables.Count*2,0);
-        _movementController.movementSpeed -= collectable.slowDown;
-    }
-    public void DropCollectable()
-    {
-        int num = _collectables.Count-1;
-        _collectables[num].transform.SetParent(null);
-        _collectables[num].transform.localPosition -= new Vector3(0,_collectables.Count*2,0);
-        _movementController.movementSpeed += _collectables[num].slowDown;
+    //public void TakeCollectable(CollectableObject collectable)
+    //{
+    //    _collectables.Add(collectable);
+    //    collectable.transform.SetParent(collectoblesParent);
+    //    collectable.transform.localPosition = new Vector3(0,_collectables.Count*2,0);
+    //    _movementController.movementSpeed -= collectable.slowDown;
+    //}
+    //public void DropCollectable()
+    //{
+    //    int num = _collectables.Count-1;
+    //    _collectables[num].transform.SetParent(null);
+    //    _collectables[num].transform.localPosition -= new Vector3(0,_collectables.Count*2,0);
+    //    _movementController.movementSpeed += _collectables[num].slowDown;
         
-        _collectables.RemoveAt(num);
-    }
+    //    _collectables.RemoveAt(num);
+    //}
     
 }
