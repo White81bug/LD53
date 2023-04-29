a4e27448a40fd154dae9b130c9a74da4d4b88fea
Alfie213 <arturbogoratov@gmail.com>
2023-04-29 20:23:23 +0300

    MonsterChasing

---
+++ Assets/Scripts/Monster/MonsterChasing.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(Collider))]
+public class MonsterChasing : MonoBehaviour
+{
+    [SerializeField, Min(0f)] private float _speed = 2f;
+
+    private NavMeshAgent _agent;
+
+    private GameObject _player;
+    private bool _isChasing;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.speed = _speed;
+        _agent.autoBraking = false;
+    }
+
+    private void Update()
+    {
+        if (_isChasing)
+        {
+            _agent.destination = _player.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<MovementController>())
+        {
+            _player = other.gameObject;
+            _isChasing = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<MovementController>())
+        {
+            _player = null;
+            _isChasing = false;
+        }
+    }
+}
